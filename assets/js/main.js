56a09e5b7ffcdb1db1f64bf22e788888e296e9da
Jagandeep Brar <me@jagandeepbrar.io>
2018-01-16 04:26:17 -0600

    Added a barebones status section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,15 @@
 /// START CONFIG ///
 var config = {
 	general: {
-		title: "Hypernova"
+		title: "Hypernova",
+		status_enabled: true
+	},
+	status: {
+		url: "https://SERVER_URL",
+		movies_num: "1200+",
+		tv_num: "200+",
+		anime_num: "50+",
+		music_num: "50+"
 	},
 	url: {
 		//Top splash blocks
@@ -28,7 +36,7 @@ var config = {
 function updatePageData() {
 	//Update page and header titles to server name
 	document.title = config.general.title;
-	document.getElementById("header-title").innerHTML += (config.general.title);
+	document.getElementById("header-title").innerHTML += config.general.title;
 	//Update top three block links
 	document.getElementById("splash-link-requests").href = config.url.requests_url;
 	document.getElementById("splash-link-plex").href = config.url.plex_url;
@@ -41,11 +49,22 @@ function updatePageData() {
 	document.getElementById("splash-link-bittorrent").href = config.url.bittorrent_url;
 	document.getElementById("splash-link-tvheadend").href = config.url.tvheadend_url;
 	document.getElementById("splash-link-netdata").href = config.url.netdata_url;
+	//Update or remove the status section
+	if(config.general.status_enabled) {
+		document.getElementById("status-text-movies-state").innerHTML += config.status.movies_num;
+		document.getElementById("status-text-tv-state").innerHTML += config.status.tv_num;
+		document.getElementById("status-text-anime-state").innerHTML += config.status.anime_num;
+		document.getElementById("status-text-music-state").innerHTML += config.status.music_num;
+	} else {
+		document.getElementById("status").style.display = "none";
+	}
 }
 
 function animate() {
 	window.sr = ScrollReveal({duration: 500});
 	sr.reveal('.header');
+	sr.reveal('.col-md-3');
+	sr.reveal('.col-md-12');
 	sr.reveal('.col-md-4');
 }
 
