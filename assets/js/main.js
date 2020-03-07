fedcb84ec4fba4da2b8a1b424369e15782c27b77
Rob Zwissler <rob@zwissler.org>
2020-03-06 18:16:25 -0800

    bump

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,8 +6,8 @@ var config = {
 	},
 	status: {
 		url: "http://10.0.46.100:32400/web/index.html",
-		movies_num: "1200+",
-		tv_num: "200+",
+		movies_num: "1161",
+		tv_num: "149",
 		anime_num: "50+",
 		music_num: "50+"
 	},
@@ -38,17 +38,12 @@ function updatePageData() {
 	document.title = config.general.title;
 	document.getElementById("header-title").innerHTML += config.general.title;
 	//Update top three block links
-	document.getElementById("splash-link-requests").href = config.url.requests_url;
 	document.getElementById("splash-link-plex").href = config.url.plex_url;
 	document.getElementById("splash-link-tautulli").href = config.url.tautulli_url;
 	//Update automation software blocks [Row 1]
 	document.getElementById("splash-link-sonarr").href = config.url.sonarr_url;
 	document.getElementById("splash-link-radarr").href = config.url.radarr_url;
 	document.getElementById("splash-link-nzbget").href = config.url.nzbget_url;
-	//Update automation software blocks [Row 2]
-	document.getElementById("splash-link-bittorrent").href = config.url.bittorrent_url;
-	document.getElementById("splash-link-tvheadend").href = config.url.tvheadend_url;
-	document.getElementById("splash-link-netdata").href = config.url.netdata_url;
 	//Update or remove the status section
 	if(config.general.status_enabled) {
 		//Update the amount of media
